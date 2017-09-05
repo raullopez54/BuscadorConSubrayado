53498be84d5d466abb64c42c66237b85160e7d45
Abravogal <alfonso@abravogal.com>
2017-09-05 21:46:33 +0200

    Initial commit

--- src/main/java/com/lab/configuration/LabConfiguration.java
+++ src/main/java/com/lab/configuration/LabConfiguration.java
@@ -1,9 +1,19 @@
 package com.lab.configuration;
 
+import javax.sql.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternUtils;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,14 +22,27 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 
 @Configuration
-@ComponentScan
+//@ComponentScan
 @RestController
+@EnableTransactionManagement
 @EnableAutoConfiguration(exclude =
 {
   DataSourceAutoConfiguration.class
 })
+@MapperScan("com.lab.persistence.mapper")
+@ComponentScan(
+        {
+          "com.lab.configuration",
+          "com.lab.persistence.service"
+        })
 public class LabConfiguration extends WebMvcConfigurerAdapter
 {
+
+  @Autowired
+  private ResourceLoader resourceLoader;
+
+  
+  
 
   @Override
   public void configurePathMatch(PathMatchConfigurer configurer)
@@ -41,6 +64,49 @@ public class LabConfiguration extends WebMvcConfigurerAdapter
             resourceChain(true).
             addResolver(new PathResourceResolver());
 
+  }
+
+
+  @Bean
+  public DataSource dataSource()
+  {
+
+//    System.out.println("---> " + env.getProperty("controller"));
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+    try
+    {
+      dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+      dataSource.setUrl("jdbc:mysql//localhost:3306/spring");
+      dataSource.setUsername("root");
+      dataSource.setPassword("");
+    }
+    catch (Exception e)
+    {
+      System.out.print("-------------->");
+    }
+    return dataSource;
+  }
+
+
+  @Bean
+  public DataSourceTransactionManager transactionManager()
+  {
+    return new DataSourceTransactionManager(dataSource());
+  }
+
+
+  @Bean
+  public SqlSessionFactoryBean sqlSessionFactory() throws Exception
+  {
+    SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+    sessionFactory.setDataSource(dataSource());
+
+//    sessionFactory.setTypeAliasesPackage("com.lab.domain");
+    sessionFactory.setMapperLocations(ResourcePatternUtils.
+            getResourcePatternResolver(resourceLoader).
+            getResources("classpath:com/lab/persistence/mapper/*.xml"));
+    return sessionFactory;
   }
 
 }
